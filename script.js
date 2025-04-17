5def939ad59ada2b26f1f8dcde189f740f1952e1
x1nk3r <olaolubalogs@gmail.com>
2025-04-17 19:13:48 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Example JavaScript for interactivity (Carousel or animations, etc.)
+document.addEventListener('DOMContentLoaded', () => {
+    // Custom JS code for interactions can be added here
+});
